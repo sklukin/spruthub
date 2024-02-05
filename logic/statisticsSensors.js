12e8f3be5a83df6078df86702fa1d9f01b331f59
sklukin <sklukin@yandex.ru>
2024-02-05 12:20:54 +0500

    option for skip sensor in statistics

--- logic/statisticsSensors.js
+++ logic/statisticsSensors.js
@@ -3,19 +3,59 @@ info = {
     description: "Отправка значений датчиков в InfluxDB",
     version: "0.1",
     author: "@sklukin",
-    onStart: false,
+    onStart: true,
 
     sourceServices: [HS.TemperatureSensor, HS.HumiditySensor],
-    sourceCharacteristics: [HC.CurrentTemperature, HC.CurrentRelativeHumidity]
+    sourceCharacteristics: [HC.CurrentTemperature, HC.CurrentRelativeHumidity],
+
+    variables: {
+        active: true,
+    },
+
+    options: {
+        SkipTemperature: {
+            name: {
+                en: "Skip temperature",
+                ru: "Не учитывать температуру"
+            },
+            type: "Boolean",
+            value: false,
+        },
+        SkipHumidity: {
+            name: {
+                en: "Skip humidity",
+                ru: "Не учитывать влажность"
+            },
+            type: "Boolean",
+            value: false,
+        },
+    },
 }
 
-function trigger(source, value) {
+function isAllowedSensor(type, options) {
+    if (type == HS.TemperatureSensor && options.SkipTemperature) {
+        return false;
+    }
+
+    if (type == HS.HumiditySensor && options.SkipHumidity) {
+        return false;
+    }
+
+    return true;
+}
+
+function trigger(source, value, variables, options) {
     const accessory = source.getAccessory();
-    const accessoryName = accessory.getName();
-    
     const service = source.getService();
-    const serviceName = service.getName();
-    const serviceType = service.getType();
+
+    if (!isAllowedSensor(service.getType(), options)) {
+        log.info("Skip get statisitics for this sensor " + source);
+        return;
+    }
+
+    const accessoryName = accessory.getName();
+    const serviceName   = service.getName();
+    const serviceType   = service.getType();
 
     const roomName = accessory.getRoom().getName();
     const tags = "room=${roomName},accessory=${accessoryName},type=${serviceType},service=${serviceName}";
